6ab5ad7961910b6be9b00bb11ed81150beb8b967
Aleksandr Gordeiko <gordeiko.ae@niuitmo.ru>
2021-11-21 14:06:46 +0300

    Fixed deployment message visibility

--- scripts/node/deploy.sh
+++ scripts/node/deploy.sh
@@ -8,4 +8,3 @@ git pull
 npm install
 npm run-script build
 systemctl start REPONAME
-curl 'https://api.telegram.org/bot1906122682:AAEjmcwMhemo0D4lXkPUJls7DBGINTXVErM/sendMessage?chat_id=339015342&text=%E2%9C%85%20Chat%20Roles%20bot%20successfully%20deployed'
