af30a56fb65d32e670a1b4c602280b7df7dbbcb0
Aleksandr Gordeiko <gordeiko.ae@niuitmo.ru>
2022-04-29 18:46:10 +0300

    Added previous service removal when redeploying

--- scripts/node/install1.sh
+++ scripts/node/install1.sh
@@ -14,6 +14,11 @@ cp "deploy.sh" "${workdir_common}/"
 cp "systemd.service" "${workdir_common}/"
 
 cd "${workdir_common}" || exit
+
+rm -rf "${repo_name}"
+rm "${ci_path}/scripts/${repo_name}-master.sh"
+rm "/etc/systemd/system/${repo_name}.service"
+
 git clone "$repo_link"
 
 workdir="${workdir_common}/${repo_name}"
@@ -26,8 +31,8 @@ sed -i -- "s:WORKDIR:${workdir}:g" systemd.service
 sed -i -- "s:USER:${ci_user}:g" systemd.service
 
 mv "deploy.sh" "${ci_path}/scripts/${repo_name}-master.sh"
-sudo chmod +x "${ci_path}/scripts/${repo_name}-master.sh"
-sudo mv "systemd.service" "/etc/systemd/system/${repo_name}.service"
+chmod +x "${ci_path}/scripts/${repo_name}-master.sh"
+mv "systemd.service" "/etc/systemd/system/${repo_name}.service"
 
 cd "${workdir}" || exit
 if [ -f "config.env.sample" ]; then
